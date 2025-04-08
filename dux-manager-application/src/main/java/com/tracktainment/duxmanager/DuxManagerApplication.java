935dd0b3f6ce23d6e89388700bd175347d8236d8
FPetronilho <jfgp_10@live.com.pt>
2025-04-08 03:18:07 +0200

    Dockerized application. Next step is to change hardcoded variables to env variables.

--- dux-manager-application/src/main/java/com/tracktainment/duxmanager/DuxManagerApplication.java
+++ dux-manager-application/src/main/java/com/tracktainment/duxmanager/DuxManagerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.tracktainment.duxmanager"})
 @EnableMongoAuditing
 public class DuxManagerApplication {
 
